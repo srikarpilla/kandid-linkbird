1561777556fc15c56a5496fdc79d00f03cc932b7
pillasrikar <pillasrikar12@gmail.com>
2025-09-08 21:41:13 +0530

    Build success fixes: updated ESLint and Turbopack config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,10 @@
-import { Pool } from 'pg';
-import * as pgCore from 'drizzle-orm/pg-core';
-
-import * as schema from './schema';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-export const db = pgCore.drizzle(pool, { schema });
 
+// Pass schema explicitly as "schema" property
+export const db = drizzle(pool, { schema });
